69d29a072aa421f8819b9efb6d9d79db8822641f
anseungwoo <asw0899@naver.com>
2022-08-17 15:08:14 +0900

    ㅠㅠ
    머지

--- uhmat_project/src/main/java/controller/RestaurantCategoryFrontController.java
+++ uhmat_project/src/main/java/controller/RestaurantCategoryFrontController.java
@@ -1,34 +1,46 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import action.*;
-import vo.*;
+import action.Action;
+import action.RestaurantDeleteAction;
+import action.RestaurantDetailAction;
+import action.RestaurantListAction;
+import action.RestaurantModifyFormAction;
+import action.RestaurantModifyProAction;
+import action.RestaurantWriteProAction;
+import action.ReviewDetailAction;
+import action.ReviewListAction;
+import action.ReviewModifyFormAction;
+import action.ReviewModifyProAction;
+import vo.ActionForward;
 
 /**
  * Servlet implementation class RestaurantReviewFrontController
  */
 @WebServlet("*.re")
 public class RestaurantCategoryFrontController extends HttpServlet {
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
+
 		String command = request.getServletPath();
 		System.out.println(command);
 		ActionForward forward = null;
 		Action action = null;
-	//-------------------------------------------------------
-		if(command.equals("/ReviewList.re")) {
-			 try {
+		// -------------------------------------------------------
+		if (command.equals("/ReviewList.re")) {
+			try {
 				action = new ReviewListAction();
-				 forward = action.execute(request, response);
+				forward = action.execute(request, response);
 			} catch (Exception e) {
-				
+
 				e.printStackTrace();
 			}
 
@@ -36,7 +48,7 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 		/*
 		 * 여기서부터는 식당 요청!! 글쓰기 수정 삭제 등은 관리자로 로그인 되어있어야함!
 		 */
-		else if(command.equals("/restaurantList.re")) {
+		else if (command.equals("/restaurantList.re")) {
 			System.out.println("식당 글 목록 요청!!");
 			try {
 				action = new RestaurantListAction();
@@ -45,23 +57,24 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(command.equals("/restaurantWriteForm.re")) {
+		} else if (command.equals("/restaurantWriteForm.re")) {
 			System.out.println("식당 글 입력 폼 요청!");
 			forward = new ActionForward();
 
 			forward.setPath("food/review/reviewWriteForm.jsp");
 
 			forward.setRedirect(false);
-		}else if(command.equals("/restaurantWritePro.re")) {
+		} else if (command.equals("/restaurantWritePro.re")) {
 			System.out.println("식당 글 입력 요청!");
 			try {
 				action = new RestaurantWriteProAction();
 				forward = action.execute(request, response);
+
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(command.equals("/restaurantDetail.re")) {
+		} else if (command.equals("/restaurantDetail.re")) {
 			System.out.println("식당 상세보기 요청!");
 			try {
 				action = new RestaurantDetailAction();
@@ -70,7 +83,7 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(command.equals("/restaurantModifyForm.re")) {
+		} else if (command.equals("/restaurantModifyForm.re")) {
 			System.out.println("식당 수정 폼 요청!");
 			try {
 				action = new RestaurantModifyFormAction();
@@ -79,7 +92,7 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(command.equals("/restaurantModifyPro.re")) {
+		} else if (command.equals("/restaurantModifyPro.re")) {
 			System.out.println("식당 수정 업데이트 요청!");
 			try {
 				action = new RestaurantModifyProAction();
@@ -88,7 +101,7 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(command.equals("/restaurantDelete.re")) {
+		} else if (command.equals("/restaurantDelete.re")) {
 			System.out.println("식당 삭제 요청!");
 			try {
 				action = new RestaurantDeleteAction();
@@ -98,42 +111,79 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
-		//추가로 태그와 카테고리 관련된 작업 요청이 더 필요함!!
+		// 추가로 태그와 카테고리 관련된 작업 요청이 더 필요함!!
 
-
-
-		 else if(command.equals("/ReviewModifyForm.re")) {
-			 try {
+		else if (command.equals("/ReviewModifyForm.re")) {
+			try {
 				action = new ReviewModifyFormAction();
-				 forward = action.execute(request, response);
+				forward = action.execute(request, response);
+
 			} catch (Exception e) {
-				
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
-		} else if(command.equals("/ReviewModifyProAction.re")) {
-			 try {
+		} else if (command.equals("/restaurantDetail.re")) {
+			System.out.println("식당 상세보기 요청!");
+			try {
+				action = new RestaurantDetailAction();
+				forward = action.execute(request, response);
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		} else if (command.equals("/ReviewModifyProAction.re")) {
+			try {
 				action = new ReviewModifyProAction();
-				 forward = action.execute(request, response);
+				forward = action.execute(request, response);
 			} catch (Exception e) {
-				
+
 				e.printStackTrace();
 			}
 
-		}else if(command.equals("/ReviewDelete.re")) {
-			 try {
+		} else if (command.equals("/ReviewDelete.re")) {
+			try {
 				action = new ReviewDetailAction();
-				 forward = action.execute(request, response);
+				forward = action.execute(request, response);
+
 			} catch (Exception e) {
-				
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} else if (command.equals("/restaurantModifyForm.re")) {
+			System.out.println("식당 수정 폼 요청!");
+			try {
+				action = new RestaurantModifyFormAction();
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (command.equals("/restaurantModifyPro.re")) {
+			System.out.println("식당 수정 업데이트 요청!");
+			try {
+				action = new RestaurantModifyProAction();
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if (command.equals("/restaurantDelete.re")) {
+			System.out.println("식당 삭제 요청!");
+			try {
+				action = new RestaurantDeleteAction();
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// 추가로 태그와 카테고리 관련된 작업 요청이 더 필요함!!
 
+		if (forward != null)
 
-		} 
-
-	
-		if (forward != null) {
+		{
 			if (forward.isRedirect()) {
 				response.sendRedirect(forward.getPath());
 			} else {
@@ -141,19 +191,23 @@ public class RestaurantCategoryFrontController extends HttpServlet {
 			}
 		}
 	}
-	//---------------------------------------------------------
+	// ---------------------------------------------------------
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
